fe828091ad00cb4d82e1f4a365aafeb3536f89b1
RobertoAntonio95 <robertoantoniomc@gmail.com>
2020-12-02 16:17:11 -0300

    cambios en manifest

--- contabilidad/__manifest__.py
+++ contabilidad/__manifest__.py
@@ -3,20 +3,25 @@
     'name': "contabilidad",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        El modulo de contabilidad esta dise~nado exclusivamente para la simplicacion de
+procesos nancieros tales como balances y la facturacion del negocio. La automatizaci
+on de este modulo permitira una facturacion rapida y agil creando cierres
+contables en plazos determinados de tiempo. Por ultimo, este modulo permite de
+forma rapida y sencilla enviar las facturas correspondiente a los clientes va correo
+electronico al momento de que estos paguen, ademas de almacenarlas en la base de
+datos para futuros analisis.""",
 
     'description': """
         Modulo encargado de la contabilidad del negocio.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Roberto Antonio Muñoz Campos",
+    'website': "http://www.utalca.cl",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Contabilidad',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +29,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
