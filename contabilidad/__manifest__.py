5c6e662b8d177a4872157f15896c34dac6c94888
RobertoAntonio95 <robertoantoniomc@gmail.com>
2020-12-03 18:35:36 -0300

    eliminé carpetas y archivos inservibles y estoy tratando de visualizar la app

--- contabilidad/__manifest__.py
+++ contabilidad/__manifest__.py
@@ -29,18 +29,12 @@
 
     # always loaded
     'data': [
-
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        'views/menu.xml',
         'views/views_vendedor.xml',
-
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
-    'installable': True,
     'application': True
 }
--- contabilidad/models/__init__.py
+++ contabilidad/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import models, vendedor
+from . import vendedor
