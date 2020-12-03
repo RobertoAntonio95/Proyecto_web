b7aa11151c965bb5836a7f1808bf5e9eb4a1d621
RobertoAntonio95 <robertoantoniomc@gmail.com>
2020-12-03 18:12:27 -0300

    se añadío modelo vendedor y vista vendedor + menu para contabilidad en las views

--- contabilidad/__manifest__.py
+++ contabilidad/__manifest__.py
@@ -29,9 +29,12 @@
 
     # always loaded
     'data': [
+
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/menu.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
