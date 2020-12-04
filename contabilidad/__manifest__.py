e08b7aaf5bcf6aa7f655075311655b28cb0f8731
RobertoAntonio95 <robertoantoniomc@gmail.com>
2020-12-04 18:48:36 -0300

    arreglado el problemagit add .git add . estaba comentado el xml de seguridad en el manifest. de momento se puede utilizar desde aqui para futuros backups ya que el programa funciona correctamente

--- contabilidad/__manifest__.py
+++ contabilidad/__manifest__.py
@@ -21,6 +21,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
+
     'category': 'Contabilidad',
     'version': '0.1',
 
@@ -29,7 +30,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views_vendedor.xml',
     ],
     # only loaded in demonstration mode
