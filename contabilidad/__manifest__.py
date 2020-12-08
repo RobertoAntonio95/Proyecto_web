7aacc78d0b08e616cdd64a462b9afc4a82d71ff0
RobertoAntonio95 <robertoantoniomc@gmail.com>
2020-12-08 16:19:49 -0300

    cambie version

--- contabilidad/__manifest__.py
+++ contabilidad/__manifest__.py
@@ -23,7 +23,7 @@
     # for the full list
 
     'category': 'Contabilidad',
-    'version': '0.1',
+    'version': '1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'ventas'],
