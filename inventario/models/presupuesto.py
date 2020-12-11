babe8c47c2fe1f656c0a36934b5dcd2f7322e265
matrvkisic <matrvkisic@gmail.com>
2020-12-10 23:32:41 -0300

    Correccion sintaxis en declaracion de tipo de variable

--- inventario/models/presupuesto.py
+++ inventario/models/presupuesto.py
@@ -9,20 +9,19 @@ class inventario(models.Model):
 
 # ACÁ LAS FK QUE UTILIZAREMOS DESDE LAS OTRAS TABLAS
 
-    vendedor_id = fields.Many2one(
-        'contabilidad.vendedor', string='vendedor')
+   # vendedor_id = fields.Many2one(
+   #     'contabilidad.vendedor', string='vendedor')
 
-    cliente_id = fields.Many2one(     #PRUEBA DE LINEA DE CODIGO
-        'ventas.cliente', string = 'cliente')
+   # cliente_id = fields.Many2one(     #PRUEBA DE LINEA DE CODIGO
+   #     'ventas.cliente', string = 'cliente')
 
-    detalle_presupuesto_id = fields.Many2one(   #FALTA CREAR DETALLE PRESP.
-        
-    )
+   # detalle_presupuesto_id = fields.Many2one(   #FALTA CREAR DETALLE PRESP.     
+   # )
 
     fecha = fields.Char(string="fecha", required=True)
     fecha_limite = fields.Date(string="fecha_limite", required=True)
     total = fields.Integer(string="total", required=True)
     estado_solicitud = fields.Boolean(string="estado_solicitud", required=True) #DUDA MAYUSCULA
-    impuesto = fields.float(string="impuesto", required=True)    # PENSANDO QUE NO FUERA UN VALOR ENTERO
+    impuesto = fields.Float(string="impuesto", required=True)    # PENSANDO QUE NO FUERA UN VALOR ENTERO
     total = fields.Integer(string="total", required=True)
     correo = fields.Char(string="correo", required=True)
