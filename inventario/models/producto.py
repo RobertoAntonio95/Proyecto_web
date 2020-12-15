1ef522a923ce78fa923907bbdd211c35ca4a17b6
RobertoAntonio95 <robertoantoniomc@gmail.com>
2020-12-15 20:34:41 -0300

    agregue el rec name en producto que lo necesitaba

--- inventario/models/producto.py
+++ inventario/models/producto.py
@@ -1,19 +1,21 @@
 from odoo import models, fields, api
 
+
 class Producto(models.Model):
 
     _name = 'inventario.producto'
     _description = 'productos'
-    
+    _rec_name = 'nombre'
 
     nombre = fields.Char(string="Nombre", required=True)
     marca = fields.Char(string="Marca", required=True)
     stock = fields.Integer(string="Stock")
     stock_critico = fields.Integer(string="Stock Crítico")
-    impuesto = fields.Boolean(string="IVA")    
+    impuesto = fields.Boolean(string="IVA")
     precio_unitario = fields.Integer(string="Precio Unitario")
     precio_final = fields.Integer(string="Precio Final")
 
-    #FK ID DEL PROVEEDOR
+    # FK ID DEL PROVEEDOR
 
-    proveedor_id = fields.Many2one('inventario.proveedor', string="ID Proveedor")
+    proveedor_id = fields.Many2one(
+        'inventario.proveedor', string="ID Proveedor")
