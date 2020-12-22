11f01da95ee1097c9aaaf819650d2e29afb137bd
RobertoAntonio95 <robertoantoniomc@gmail.com>
2020-12-22 19:21:44 -0300

    comenté cosas que daban error en prod.py y prod.xml

--- inventario/models/producto.py
+++ inventario/models/producto.py
@@ -5,7 +5,7 @@ class Producto(models.Model):
 
     _name = 'inventario.producto'
     _description = 'productos'
-    
+
     _rec_name = 'nombre'
 
     nombre = fields.Char(string="Nombre", required=True)
@@ -15,20 +15,20 @@ class Producto(models.Model):
     impuesto = fields.Boolean(string="IVA", required=True)
     precio_costo = fields.Integer(string="Precio Costo", required=True)
     precio_venta = fields.Integer(string="Precio Venta", required=True)
-    image = fields.Binary(string="Imagen") #PROBANDO IMAGENES
+    image = fields.Binary(string="Imagen")  # PROBANDO IMAGENES
 
     # FK ID DEL PROVEEDOR
 
     proveedor_id = fields.Many2one(
         'inventario.proveedor', string="ID Proveedor")
-    
-    #producto_id = fields.Char(string="SKU", unique=True) #ACÁ PODRIA SER EL CODIGO DE BARRA O SKU DE CADA PRODUCTO
-    
-    id_detalle_orden_compra= fields.One2many('ventas.detalle_compra', 'producto_ids', string="Producto")
-    
-    producto_id = fields.One2many(
-        'contabilidad.detalle', 'producto_id', string='producto_id')
 
+    # producto_id = fields.Char(string="SKU", unique=True) #ACÁ PODRIA SER EL CODIGO DE BARRA O SKU DE CADA PRODUCTO
 
+    id_detalle_orden_compra = fields.One2many(
+        'ventas.detalle_compra', 'producto_ids', string="Producto")
 
-    _sql_constraints = [   ('producto_unique', 'unique(producto_id)', 'El SKU no puede coincidir con un producto ya existente en inventario'), ]   
+    # producto_id = fields.One2many(
+    #     'contabilidad.detalle', 'producto_id', string='producto_id')
+
+    _sql_constraints = [('producto_unique', 'unique(producto_id)',
+                         'El SKU no puede coincidir con un producto ya existente en inventario'), ]
